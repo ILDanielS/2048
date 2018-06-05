9e7d4df38bcb82084f7c7c2db054300f77b15755
Daniel Krigel <daniel.krigel@gmail.com>
2018-06-05 08:00:22 +0300

    Took some of the methods from game.py to a new manager class

---
+++ manager_console.py
@@ -0,0 +1,37 @@
+import sys
+import os
+import game as g
+import const
+
+class ConsoleManager:
+
+    def __init__(self, player):
+        self.__state = g.Game()
+        self.__player = player
+
+    def print_board(self):
+        board = self.__state.get_board()
+        os.system('clear')
+        print('-'*const.BOARD_COLUMNS*5)
+        for (i, j) in const.TILES_UP_ORDER:
+            if board[(i, j)] == const.EMPTY:
+                print(repr(0).rjust(4), end="|")
+            else:
+                print(repr(board[(i, j)].get_value()).rjust(4), end="|")
+            if j+1 == const.BOARD_COLUMNS:
+                print("\n"+'-'*const.BOARD_COLUMNS*5)
+
+
+    def start_game(self):
+        self.__state.generate_tile(const.START_TILES)
+        next_possibles_moves = self.__state.get_next_moves()
+        while not next_possibles_moves:
+            self.print_board()
+            direction = player.get_move()
+            if direction not in const.DIRECTION_LIST or \
+               direction not in next_possibles_moves:
+                continue
+            self.__state.make_move(direction)
+            self.__state.generate_tile()
+            next_possibles_moves = self.__state.get_next_moves()
+        self.print_board()
