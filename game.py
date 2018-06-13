6a0bf1c1d4683da0acccabb66e8e12ee70d44347
Daniel Krigel <daniel.krigel@gmail.com>
2018-06-13 09:06:31 +0300

    MANY CHANGESSS

--- game.py
+++ game.py
@@ -6,6 +6,8 @@ from copy import deepcopy
 from const import *
 import utils
 
+from time import time
+
 class Game:
     __score = 0
     __startTiles = 2
@@ -16,6 +18,8 @@ class Game:
                         for j in range(BOARD_ROWS)}
         self.__verbose = verbose_flag
         self.__turn = Turn.PLAYER
+        self.__gen_all_possible_moves = False
+        self.__gen_all_possible_tiles = False
 
     def _gen_tile_at(self, place, number):
         self.__board[place] = Tile(number)
@@ -38,7 +42,11 @@ class Game:
                 pos = empty_tiles.pop()
                 self.__board[pos] = Tile()
                 self.__board[pos].set_random_value()
-                self.__score += self.__board[pos].get_value()
+                # self.__score += self.__board[pos].get_value()
+                self.__turn = Turn.PLAYER
+
+        self.__gen_all_possible_moves = False
+        self.__gen_all_possible_tiles = False
 
     def __is_move_possible(self, tile_order, prev_tile_func):
         for curr_pos in filter(lambda x: self.__board[x] != EMPTY, tile_order):
@@ -73,7 +81,7 @@ class Game:
                                           EMPTY, self.__board[curr_pos]
             else:
                 self.__board[farthest_pos].merge()
-                self.__score += self.__board[curr_pos].get_value()
+                self.__score += self.__board[curr_pos].get_value()*2
                 self.__board[curr_pos] = EMPTY
 
     # Is the state is a final state
@@ -87,6 +95,15 @@ class Game:
         return self.__score
 
     def get_all_possible_tile_gen(self):
+        start_time = time()
+        if self.__gen_all_possible_moves:
+            # print("---- %s seconds ----" %(time() - start_time))
+            return self.__all_possible_moves
+
+        current_state = deepcopy(self)
+        current_state.__all_possible_moves = []
+        current_state.__all_possible_tiles = []
+
         possible_tile_gen_list = []
         for pos in self.__get_empty_tiles():
             tmp_state = deepcopy(self)
@@ -95,25 +112,49 @@ class Game:
             possible_tile_gen_list.append((0.8/16, tmp_state))
             tmp_state._gen_tile_at(pos, 4)
             possible_tile_gen_list.append((0.2/16, tmp_state))
+
+        self.__all_possible_moves = possible_tile_gen_list
+        self.__gen_all_possible_moves = True
+        endtime = time()
+        # print ()
         return possible_tile_gen_list
 
 
     def get_all_possible_moves(self):
+        start_time = time()
+        if self.__gen_all_possible_tiles:
+            # print("---- %s seconds ----" %(time() - start_time))
+            return self.__all_possible_tiles
+
+        current_state = deepcopy(self)
+        current_state.__all_possible_moves = []
+        current_state.__all_possible_tiles = []
+
         possible_moves = self.get_next_moves()
         states_after_moves_list = []
         for move in possible_moves:
-            tmp_state = deepcopy(self)
+            tmp_state = deepcopy(current_state)
             tmp_state.__turn = Turn.COMPUTER
             tmp_state.make_move(move)
             states_after_moves_list.append((move, tmp_state))
+        self.__gen_possible_tiles = True
+        self.__all_possible_tiles = states_after_moves_list
+        # print("---- %s seconds ----" %(time() - start_time))
         return states_after_moves_list
 
 
     def get_board(self):
-        return self.__board
+        ret_board = {}
+        for pos in self.__board:
+            if self.__board[pos] == EMPTY:
+                ret_board[pos] = 0
+            else:
+                ret_board[pos] = self.__board[pos].get_value()
+        return ret_board
 
 
     def get_next_moves(self):
+        #TODO: add a cashe in order to not search twice per state
         moves = []
         for direction in DIRECTION_LIST:
             tiles_order = TILE_ORDER_DICT[direction]
@@ -143,11 +184,16 @@ class Game:
         self.__move_tiles(tiles_order, direction)
         for pos in filter(lambda pos: self.__board[pos] != EMPTY, tiles_order):
             self.__board[pos].reset_tile_round_end()
+        self.__turn = Turn.COMPUTER
+        self.__gen_all_possible_moves = False
+        self.__gen_all_possible_tiles = False
+        self.__all_possible_moves = []
+        self.__all_possible_tiles = []
 
     def start_game_human(self):
         self.generate_tile(START_TILES)
         next_possibles_moves = self.get_next_moves()
-        
+
         while next_possibles_moves:
             self.print_board()
             direction = sys.stdin.read(1)
